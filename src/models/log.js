8ffffdd9a50879132687c159ff959856cbfca5f2
Gabriel de Oliveira Barbosa <manobi.oliveira@gmail.com>
2012-02-21 12:40:45 -0200

    fix keys path

--- src/models/log.js
+++ src/models/log.js
@@ -2,7 +2,7 @@
  * @class Log
  */
 var Log = function(){
-	var keys = require('./keys').keys;
+	var keys = require('../keys').keys;
 	this.campaign_id = "";
 	this.site_id = keys.site;
 	this.page_id = keys.page;
