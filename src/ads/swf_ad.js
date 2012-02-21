7ce0e406ee889f3338dd7da6109fa1c8b47a1d0c
Gabriel de Oliveira Barbosa <manobi.oliveira@gmail.com>
2012-02-21 21:14:28 -0200

    cleaning flash ad

--- src/ads/swf_ad.js
+++ src/ads/swf_ad.js
@@ -13,19 +13,50 @@
 var SwfAd = function(id,file,link, width,height,campaign){	
 	var Ad = require('./ad').ad;
 	Ad.apply(this, arguments);
+	var EventEmitter = require('../../node_modules/events').events.EventEmitter;
+	EventEmitter.apply(this, arguments);
+	var connection = require('../../connections').connections;
+	var Impression = require('../../models/impression').Impression;
+	var queryString = require('../../node_modules/querystring').querystring;
 	
 	var ObjectTag = require('../html/flash/object_tag').object_tag;
 	var EmbedTag = require('../html/flash/embed_tag').embed_tag;
 	
+	
 	this.element = {};
-	var __construct = function(_self){
+	/** 
+	* @public
+	* @return {String}
+	*/
+	this.getSrc = function(){
+		var preloaderUrl = connection['static'].getUrl() + '/lib/as3.swf';
+		var query = queryString.stringfy({
+			"src":this.src,
+			"ad_id":this.id,
+			"link":this.link
+		});
+		return preloaderUrl + "?" +query;
+	};
+	var __construct = (function(_self){
+		
 		if(browser.msie){
 			_self.element = new ObjectTag(id,file,link,width,height,campaign);
 		} else{
 			_self.element = new EmbedTag(id,file,link,width,height,campaign);
 		}
-
-	}(this);
+		// todo: migrate this to class ad or swfAd
+		_self.addListener("load", function(){
+			var print = new Impression();
+			print.campaign_id = _self.rel;
+			print.space_id = _self.element.parentNode.id;
+			print.ad_id = _self.id;
+			print.save();
+		});
+		
+		// exporting this instance to ads namespace
+		//todo: remove this from this place to work in node
+		Ad.ads[_self.id] = _self;
+	})(this);
 	return this.element;
 };
 exports.swf_ad = SwfAd;
--- src/html/flash/flash.js
+++ src/html/flash/flash.js
@@ -4,13 +4,8 @@
  * */
 var Flash = function(id,src,link,width,height,rel){
 	var HtmlTag = require('../html_tag').html_tag;
-	var EventEmitter = require('../../node_modules/events').events.EventEmitter;
 	HtmlTag.apply(this, arguments);
-	EventEmitter.apply(this, arguments);
-	
-	var connection = require('../../connections').connections;
-	var Impression = require('../../models/impression').Impression;
-	var queryString = require('../../node_modules/querystring').querystring;
+
 	
 	this.id = id;
 	this.rel = rel;
@@ -30,33 +25,8 @@ var Flash = function(id,src,link,width,height,rel){
 	//this.allowNetworking = "all";
 	this.link = "";	
 	
-	/** 
-	* @public
-	* @return {String}
-	*/
-	this.getSrc = function(){
-		var preloaderUrl = connection['static'].getUrl() + '/lib/as3.swf';
-		var query = queryString.stringfy({
-			"src":this.src,
-			"ad_id":this.id,
-			"link":this.link
-		});
-		return preloaderUrl + "?" +query;
-	};
+
 	
-	var __construct = (function(_self){
-		// todo: migrate this to class ad or swfAd
-		_self.addListener("load", function(){
-			var print = new Impression();
-			print.campaign_id = _self.rel;
-			print.space_id = _self.element.parentNode.id;
-			print.ad_id = _self.id;
-			print.save();
-		});
-		
-		// exporting this instance to ads namespace
-		//todo: remove this from this place to work in node
-		Ad.ads[_self.id] = _self;
-	})(this);
+
 };
 exports.flash = Flash;
