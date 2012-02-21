978309754b25452c2762fd316aac8da5fb416c9f
Gabriel de Oliveira Barbosa <manobi.oliveira@gmail.com>
2012-02-21 14:29:45 -0200

    salvable.save privileged

--- src/models/salvable.js
+++ src/models/salvable.js
@@ -3,17 +3,18 @@
  * @todo: ver se é melhor passa o site por aqui ou no server
  */
 
-var Salvable = function () {};
-Salvable.prototype.save = function(){
+var Salvable = function () {
 	var connections = require('../connections').connections;
-	var query = {
-		//ad_id:this.ad_id,
-		campaign_id:this.campaign_id,
-		space_id:this.space_id,
-		page_url:this.page_url,
-		site_id: this.site_id,
-		page_id: this.page_id
+	this.save = function(){
+		var query = {
+			//ad_id:this.ad_id,
+			campaign_id:this.campaign_id,
+			space_id:this.space_id,
+			page_url:this.page_url,
+			site_id: this.site_id,
+			page_id: this.page_id
+		};
+		connections.impressions.request().img('/impression/' + this.ad_id, query);
 	};
-	connections.impressions.request().img('/impression/' + this.ad_id, query);
 };
 exports.Salvable = Salvable;
