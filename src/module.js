92f1204a28ecfd88a0d7689b734ff538c8268dbb
Gabriel de Oliveira Barbosa <manobi.oliveira@gmail.com>
2012-02-21 12:59:05 -0200

    comments of common module

--- src/module.js
+++ src/module.js
@@ -1,5 +1,3 @@
-/** Copyright 2011 Adlayer Adserver. All Rights Reserved. */
-
 /**
  * @private
  * Implementation of common.js
@@ -13,7 +11,7 @@ var exports = module.exports;
 /**
  * @example
  * EventEmitter = require('events').EventEmitter;
- * No mattter what you pass parameter, this will always find for last dirname, bacause in this implementation
+ * No mattter what you pass parameter, this will always find for var 'exports' , bacause in this implementation
  * Everything are in the same structure
  */
 var require = function(path){
