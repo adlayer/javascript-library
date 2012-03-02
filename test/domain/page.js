9243ae37fc25d9483b1d4dd76d9226ec17af5e03
Gabriel de Oliveira Barbosa <manobi.oliveira@gmail.com>
2012-03-02 01:19:00 -0300

    page.js tests

--- test/domain/page.js
+++ test/domain/page.js
@@ -1,3 +1,27 @@
 describe('Page', function(){
-	
+	describe('#getActiveContent', function(){
+		it('should filter all content by status true', function(){
+			
+			var space1 = new Space({
+				id: '1',
+				status: true,
+				ads: [
+					new Ad({status:true}), 
+					new Ad({status:false})
+				]
+			});
+
+			var page = new Page({
+				id: '123',
+				name: 'TestUnit',
+				spaces: [space1],
+				status: true
+			});
+			
+			var result = page.getActiveContent();
+			var adsInSpace1 = result.spaces[0]['ads'];
+			expect(adsInSpace1.length).to.be(1);
+			
+		});
+	});
 });
