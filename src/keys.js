728ae56f6a434d20d52958060e4f8753bd1c3734
Gabriel de Oliveira Barbosa <manobi.oliveira@gmail.com>
2012-06-13 03:12:05 -0300

    remove old files

--- src/keys.js
+++
@@ -1,9 +0,0 @@
-/*
-* @todo: Should be a class
-* @todo: Find and remove many reference to this module
-*/
-exports.keys = {
-	page: '',
-	site: '',
-	domain: ''
-};
--- src/spaces/ispace.js
+++
@@ -1,8 +0,0 @@
-/**
-* @interface ISpace
-*/
-var ISpace = function(){
-	this.render = function(){ return new Error("not implemented");};
-	this.insertAd = function(){return new Error("not implemented");};
-};
-exports.ispace = ISpace;
