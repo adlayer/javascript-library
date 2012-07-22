1844626208fd54aa2c90c5eb5ffda103cbbcd7bd
Gabriel de Oliveira Barbosa <manobi.oliveira@gmail.com>
2012-07-22 11:53:04 -0300

    using strategy pattern to set space behaviour

--- src/domain/space.js
+++ src/domain/space.js
@@ -1,4 +1,34 @@
 /**
+* Interface for space behaviour
+*
+* @interface ISpaceBehaviour
+* @constructor
+*/
+var ISpaceBehaviour = function(){
+	this.getAd = function(context){ throw new Error('Implement it'); };
+};
+exports.ISpaceBehaviour = ISpaceBehaviour;
+
+/**
+* Random behaviour for spaces
+*
+* @class RandomBehaviour
+* @constructor
+*/
+var RandomSpaceBehaviour = function(){
+	/**
+	* @method getAd
+	* @param {Space} context Expect space 'this' as argument
+	* @return {Object} Ad
+	*/
+	this.getAd = function(context){ 
+		var total = context.ads.length;
+		var index = Math.floor(Math.random() * total);
+		return context.ads[index];
+	};
+};
+
+/**
 * Abstract class for spaces
 *
 * @class Space
@@ -24,15 +54,29 @@ var Space = function( attributes ){
 	* @property {Array} ads Collection of ads linked to space
 	*/
 	this.ads = [];
+	
+	/**
+	* @property {SpaceBehaviour} behaviour a part of strategy pattern
+	*/
+	this.behaviour = {};
 
 	/**
-	* @method getRandomAd
+	* @method setBehaviour
+	* @param {Object} behaviour 
 	* @return {Object} Ad
 	*/
-	this.getRandomAd = function(){
-		var total = this.ads.length;
-		var index = Math.floor(Math.random() * total);
-		return this.ads[index];
+	this.setBehaviour = function(behaviour){
+		this.behaviour = behaviour;
+		return this.behaviour;
+	};
+	
+		
+	/**
+	* @method getAd
+	* @return {Object} Ad
+	*/
+	this.getAd = function(){
+		return this.behaviour.getAd(this);
 	};
 	
 	/*
@@ -42,6 +86,7 @@ var Space = function( attributes ){
 	*/
 	var __construct = function(self){
 		self = self.extend(attributes);
+		self.setBehaviour(new RandomSpaceBehaviour());
 	}(this);
 };
 
--- test/domain/space.js
+++ test/domain/space.js
@@ -1,9 +1,49 @@
 var expect = expect || require('expect.js');
 var Ad = require('../../src/domain/ad.js').Ad;
 var Space = require('../../src/domain/space.js').Space;
+var ISpaceBehaviour = require('../../src/domain/space.js').ISpaceBehaviour;
+
+describe('ISpaceBehaviour', function(){
+	describe('#getAd', function(){
+		it('Should throw an error', function(){
+			
+			var behaviour = new ISpaceBehaviour();
+			expect(behaviour.getAd).to.throwError('Implement it');
+
+		});
+	});
+});
 
 describe('Space', function(){
-	describe('#getRandomAd', function(){
+	describe('#setBehaviour', function(){
+		it('Should set a behaviour in the created space', function(){
+			
+			var FirstBehaviour = function(){
+				ISpaceBehaviour.apply(this, arguments);
+				
+				this.getAd = function(context){
+					if(context){
+						return context.ads[0];
+					}
+				}
+			};
+			
+			var space = new Space({
+				ads: [
+					new Ad({id:1}),
+					new Ad({id:2})
+				]
+			});
+			var firstBehaviour = new FirstBehaviour();
+			
+			expect(firstBehaviour.getAd).to.not.throwError();
+			
+			space.setBehaviour(firstBehaviour);
+			
+			expect( space.getAd() ).to.be.equal(space.ads[0]);
+		});
+	});
+	describe('#getAd', function(){
 		it('Should return a random ad', function(){
 			
 			var space = new Space({
@@ -12,7 +52,7 @@ describe('Space', function(){
 					new Ad({id:2})
 				]
 			});
-			expect(space.getRandomAd()).to.be.a(Ad);
+			expect(space.getAd()).to.be.a(Ad);
 		});
 	});
 });
